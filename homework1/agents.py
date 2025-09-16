b3fab2ab7482ca78e14794f99d6774dde80bfe7b
Misha Awad <awadmishaa@gmail.com>
2025-09-15 21:47:19 -0400

    create random bot functionality

--- homework1/agents.py
+++ homework1/agents.py
@@ -1,15 +1,53 @@
+#**************************************************************************
+# Results:
+# RandomAgent: 
+# 	Mean: 238.0
+# 	STDDev: N/A
+# ReflexAgent:
+# 	Mean: 
+# 	STDDev: 
+#**************************************************************************
+
 from game import Agent
 from game import Directions
 from game import Actions
 from random import random
 
+
 class BasicAgent(Agent):
   """This agents just heads North until it cannot move"""
   def getAction(self, state):
-    #return Directions.NORTH --Exception: Illegal action North
-    #return state.NORTH --AttributeError: 'GameState' object has no attribute 'NORTH'
-    #return state.getLegalPacmanActions() #--Exception: Illegal action ['South', 'West', 'Stop']
-    random_num = int(len(state.getLegalPacmanActions()) * random())
-    return state.getLegalPacmanActions()[random_num]
+    # a legal pacman action from the array is chosen, thus no error codes returned
+    #however, it becomes an infinite action, so do not run
+    #return state.getLegalPacmanActions()[0]
     
-    
+    #random agent
+        #create a random number based on the length of state.getLegalPacmanActions()
+        #arr to index the array with at the call
+        
+        #index the state.getLegalPacmanActions() arr with the random int
+        
+        #make it so it only chooses stop if others not available
+        #get current positoin of pacman
+        
+        #If the length of the list of legal actions is more than a single option
+        if len(state.getLegalPacmanActions()) > 1:
+            #remove any option that equals stop if it exists
+            filtered = list(filter(lambda x: x != "Stop", state.getLegalPacmanActions()))
+            
+            #get the length of that filtered list and pick a random number
+            #from zero to the length to index with
+            random_num = int(len(filtered) * random())
+            
+            #index the filtered list with the randomly selected index
+            choice = filtered[random_num]
+            print(choice)                
+            return choice
+        #if the length of options is only one, return that value
+        else:
+            choice = state.getLegalPacmanActions()[0]
+            return choice
+            print("length less than 2"+choice)
+        
+            
+        
